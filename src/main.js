8e38e38198e5120846ee09208c247df287e321fa
Madison Beatty <madison.beatty@valpo.edu>
2021-05-04 20:16:36 -0500

    issue with images not loading on build

--- src/main.js
+++ src/main.js
@@ -1,37 +1,32 @@
 import Phaser from "phaser";
 // import TitleScreen from './scenes/TitleScreen'
-// import Game from './scenes/Game'
+import Game from './scenes/Game'
+
 
 var config = {
   width: 800,
   height: 500,
-  type: Phaser.AUTO,
-  scene:{
-    preload: preload,
-    create: create,
-    update: update
-  }
+  type: Phaser.AUTO
 };
 
-var game = new Phaser.Game(config)
+var game = new Phaser.Game(config);
 
-function preload(){
-  this.load.image('heat', './assets/Heat.png')
-  this.load.image('ice', './assets/Ice.png')
-  this.load.image('pot', './assets/Pot.png')
-  this.load.image('water', './assets/Water.png')
-  this.load.image('wind', './assets/Wind.png')
-}
+// function preload() {
+//   this.load.image("heat", "./assets/Heat.png");
+//   this.load.image("ice", "./assets/Ice.png");
+//   this.load.image("pot", "./assets/Pot.png");
+//   this.load.image("water", "./assets/Water.png");
+//   this.load.image("wind", "./assets/Wind.png");
+//   this.load.image("city", "/Users/madisonbeatty/stormBrewer/assets/City.png");
+// }
 
-function create(){
-
-}
-function update(){
-
-}
+// function create() {
+//   this.add.image(400, 300, "city").setOrigin(0, 0);
+// }
+// function update() {}
 
 // game.scene.add('titlescreen', TitleScreen)
-// game.scene.add('game', Game )
+game.scene.add('game', Game )
 
-// // // game.scene.start('titlescreen')
-// game.scene.start('game')
+// game.scene.start('titlescreen')
+game.scene.start('game')
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,10 +2,10 @@ import Phaser from 'phaser'
 
 export default class Game extends Phaser.Scene {
     preload(){
-        this.load.image('City', './assets/City.png')
+        this.load.image('wind', 'assets/Wind.png')
     }
 
     create(){
-        this.add.image(400,250, 'City')
+        this.add.image(400,250,'wind')
     }
 }
