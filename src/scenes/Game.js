e114954544b6e2fa08234946f5b4d5954ec92fa2
Madison Beatty <madison.beatty@valpo.edu>
2021-06-15 23:41:09 -0500

    added city and cooking pot pic

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,8 +3,14 @@ const Phaser = require("phaser");
 class Game extends Phaser.Scene {
   preload() {
     this.load.image("wind", require("../../public/assets/Wind.png"));
+    this.load.image(
+      'city', require(("../../public/assets/City.png")) 
+    );
+    this.load.image('pot', require("../../public/assets/Pot.png"));
   }
   create() {
+    this.add.image(300, 800, 'city');
+    this.add.image(50, 440, 'pot');
     const text = this.add.text(400, 250, "hello world");
     text.setOrigin(0.5, 0.5);
     this.add.image(400, 250, "wind");
